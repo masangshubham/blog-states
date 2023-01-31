b480e170473aa51ee8fddf100880415887e15272
masangshubham <shubhamm_t20018@students.isquareit.edu.in>
2023-01-31 12:07:34 +0530

    SSR user profile page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,103 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import PostFeed from "../components/PostFeed";
+import Metatags from "../components/Metatags";
+import Loader from "../components/Loader";
+import { firestore, postToJSON, getIt } from "../lib/firebase";
+import {
+  Timestamp,
+  query,
+  where,
+  orderBy,
+  limit,
+  collectionGroup,
+  getDocs,
+  startAfter,
+  getFirestore,
+} from "firebase/firestore";
+
+import { useState } from "react";
 
 
+const LIMIT = 1;
 
-export default function Home() {
+export async function getServerSideProps(context) {
+  const ref = collectionGroup(getFirestore(), "posts");
+  const postsQuery = query(
+    ref,
+    where("published", "==", true),
+    orderBy("createdAt", "desc"),
+    limit(LIMIT)
+  );
+
+  const posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+
+  return {
+    props: { posts },
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor =
+      typeof last.createdAt === "number"
+        ? Timestamp.fromMillis(last.createdAt)
+        : last.createdAt;
+
+    const ref = collectionGroup(getFirestore(), "posts");
+    const postsQuery = query(
+      ref,
+      where("published", "==", true),
+      orderBy("createdAt", "desc"),
+      startAfter(cursor),
+      limit(LIMIT)
+    );
+
+    const newPosts = (await getDocs(postsQuery)).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <main>
+      <Metatags
+        title="Home Page"
+        description="Get the latest posts on our site"
+      />
+
+      {/* <div className="card card-info">
+        <h2>💡 Next.js + Firebase - The Full Course</h2>
+        <p>
+          Welcome! This app is built with Next.js and Firebase and is loosely
+          inspired by Dev.to.
+        </p>
+        <p>
+          Sign up for an 👨‍🎤 account, ✍️ write posts, then 💞 heart content
+          created by other users. All public content is server-rendered and
+          search-engine optimized.
+        </p>
+      </div> */}
+
+      <PostFeed posts={posts} />
+
+      {!loading && !postsEnd && (
+        <button onClick={getMorePosts}>Load more</button>
+      )}
+
+      <Loader show={loading} />
+
+      {postsEnd && "You have reached the end!"}
+    </main>
+  );
 }
